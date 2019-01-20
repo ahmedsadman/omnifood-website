404bcb2cd96724ef8201dbcf9317f0436c8c6492
Ahmed Sadman <ahmedsadman.211@gmail.com>
2019-01-21 00:10:33 +0600

    sticky navigation switch based on scroll direction, for smaller screens

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,6 +24,20 @@ const fadeIn = (el, duration) => {
     tick();
 };
 
+// get scroll direction
+let scrollDirection;
+let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop){
+       scrollDirection = 'down';
+    } else {
+       scrollDirection = 'up'
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    console.log(scrollDirection);
+});
+
 const fadeOut = (el, duration) => {
     return new Promise((resolve, reject) => {
         el.style.opacity = 1;
@@ -42,8 +56,8 @@ const fadeOut = (el, duration) => {
     });
 };
 
-// Hide/Show sticky footer
-const waypoint = new Waypoint({
+// Hide/Show sticky footer (for large screens)
+const toggleStickyDefault = new Waypoint({
     element: elements.featuresSectionElement,
     handler: function(direction) {
         if (direction === 'down') {
@@ -55,6 +69,29 @@ const waypoint = new Waypoint({
                 elements.navElement.style.opacity = 1.0;
             });
         }
+
+        if (direction === 'up' && document.documentElement.clientWidth <= 520) {
+            elements.navElement.classList.remove('sticky');
+            elements.navElement.style.opacity = 1.0;
+        }
     },
     offset: 80
+});
+
+// disable the default sticky nav trigger on waypoint
+console.log(document.documentElement.clientWidth);
+if (document.documentElement.clientWidth <= 520) {
+    toggleStickyDefault.disable();
+}
+
+// hide show sticky footer based on scroll direction, for small screens
+window.addEventListener('scroll', () => {
+    if (document.documentElement.clientWidth <= 520) {
+        if (scrollDirection === 'down' || lastScrollTop <= 400) {
+            elements.navElement.classList.remove('sticky');
+            elements.navElement.style.opacity = 1.0; 
+        } else {
+            elements.navElement.classList.add('sticky');
+        }
+    }
 });
